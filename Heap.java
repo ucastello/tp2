3324d62790d83ab38c4b242dbe1599d16d4adb36
BGold10 <143293705+BGold10@users.noreply.github.com>
2024-11-15 18:07:38 -0300

    Add files via upload

---
+++ Heap.java
@@ -0,0 +1,135 @@
+package aed;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+ 
+public class Heap<T> {
+    private ArrayList<T> data;
+    private int ultimo;
+    private Comparator<T> comparador;
+
+    public Heap (ArrayList<T> info, Comparator<T> com){
+        comparador = com;
+        data = info;
+        ultimo = data.size()-1;
+        this.Heapify();
+    }
+
+    public Heap (Heap<T> h, Comparator<T> comp){                                //Constructor por copia
+        comparador = comp;
+        ultimo = h.longitud()-1;
+        data = new ArrayList<T>();
+        int i = 0;
+        while (i <= ultimo){
+            data.add(h.obtener(i));
+            i++;
+        }
+    }
+
+    public T maximo (){
+        return data.get(0);
+    }
+
+    public T eliminarPosicion (int i){
+        T res = data.get(i);
+        if (i == ultimo){
+            data.remove(i);
+        }
+        else if (esHoja(i)){
+            intercambiar(i, ultimo);
+            data.remove(ultimo);
+        } 
+        else if (ultimo == i+1){
+            intercambiar(i, ultimo);
+            data.remove(ultimo);
+        }
+        else{
+            intercambiar(i, ultimo);
+            data.remove(ultimo);
+            if (hijosConMasPrioridad(i)){
+                bajar(i);
+            }
+            else if (comparador.compare(data.get(i), data.get((i-1)/2)) > 0){              //si quiero eliminar la raiz a este else no entra nunca, creo que no habria problema igual
+                subir(i);
+            }
+        }
+        ultimo = data.size()-1;
+        return res;
+    }
+
+    public int longitud(){
+        return data.size();
+    }
+
+    public T obtener(int indice){
+        return data.get(indice);
+    }
+
+    public void agregar (T elem){
+        if (data.size()== 0){
+            data.add(0,elem);
+            ultimo += 1;
+        } else {
+            data.add(ultimo+1,elem);
+            ultimo +=1;
+            subir(ultimo);
+        }
+        
+    }
+        
+    private void Heapify(){
+        int i = data.size()/2-1;
+        while (i >= 0){
+            bajar(i);
+            i--;
+        }
+    }
+
+    private Boolean esHoja(int indice){
+        return (2*indice+1 > (data.size()-1));
+    }
+
+    private void intercambiar (int Indice1,int Indice2){
+        T num = data.get(Indice1);
+        data.set(Indice1, data.get(Indice2));
+        data.set(Indice2, num);
+    }
+
+    private Boolean hijosConMasPrioridad (int indice){                                          // devuelve True si tiene algun hijo con mas prioridad
+        if ( 2*indice+2 > (data.size()-1)){
+            return (comparador.compare(data.get(indice), data.get(2*indice+1)) < 0);
+        }
+        return ((comparador.compare(data.get(indice), data.get(2*indice+1)) < 0) || (comparador.compare(data.get(indice), data.get(2*indice + 2)) < 0));
+    }
+
+    private int hijoMayorPrioridad (int indice){
+        int res = 2*indice+1;
+        if ((2*indice+2 >= data.size())){
+            return res;
+        }
+        else if (comparador.compare(data.get(2*indice+1), data.get(2*indice+2))<0){
+            res = 2*indice +2;
+        }
+        return res;
+    }
+
+    private void bajar(int i){
+        while (!esHoja(i) && hijosConMasPrioridad(i)) {
+            int hijoMayorPrioridad = hijoMayorPrioridad(i);
+            intercambiar(i, hijoMayorPrioridad);
+            i = hijoMayorPrioridad;
+        }
+    }
+    
+    private void subir (int i){
+        while (i >1 && (comparador.compare(data.get(i), data.get(i/2))>0)){             
+            intercambiar(i, (i-1)/2);
+            i = (i-1)/2;
+        }
+        if (comparador.compare(data.get(1), data.get(0))>0){                
+            intercambiar(1, 0);
+        }
+    }
+
+}
+
---
+++ HeapTest.java
@@ -0,0 +1,161 @@
+package aed;
+
+import static org.junit.jupiter.api.Assertions.*;
+ 
+import java.beans.Transient;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class HeapTest {
+    private Heap<Integer> Heap;
+    private Heap<Integer> oldHeap;
+
+    @BeforeEach
+    void setUp(){
+    Comparator<Integer> comp = Integer::compare;
+    ArrayList<Integer> info = new ArrayList<>();
+    info.add(3);
+    info.add(8);
+    info.add(2);
+    info.add(5);
+    info.add(6);
+    info.add(1);
+    info.add(9);
+    info.add(7);
+    Heap = new Heap<Integer>(info, comp); 
+    oldHeap = new Heap<>(Heap, Integer::compare);
+
+    }
+
+
+    private boolean pertenece (Heap<Integer> h, Integer elem){
+        boolean res = false;
+        int i = 0;
+        while (i < h.longitud()) {
+            if (h.obtener(i) == elem){
+                res = true;
+            }
+            i++;
+        }
+        return res;
+    }
+
+    private boolean esColaDePrioridad (Heap<Integer> h){
+        boolean res = true;
+        Integer elemAnterior =h.eliminarPosicion(0);
+        while (h.longitud()>0) {
+            if (h.obtener(0)> elemAnterior){
+                res = false;
+            }
+            elemAnterior = h.eliminarPosicion(0);
+        }
+        return res;
+    }
+
+    @Test 
+    void vacio(){
+        Comparator<Integer> comparador = Integer::compare;
+        ArrayList<Integer> lista = new ArrayList<>();
+        Heap<Integer> h = new Heap<Integer>(lista, comparador);
+        assertTrue(h.longitud() == 0);
+    }
+    
+    @Test
+    void eliminarMaximoUnaVez(){
+        Heap.eliminarPosicion(0);
+        assertTrue(oldHeap.longitud() == Heap.longitud()+1);
+        
+        int i = 0;
+        while (i< Heap.longitud()) {
+            assertTrue(oldHeap.obtener(0)>Heap.obtener(i));
+            i++;
+        }
+        assertTrue(esColaDePrioridad(Heap));
+    }
+
+    @Test
+    void agregarUnElem(){
+        Heap.agregar(10);
+        assertTrue(oldHeap.longitud() +1 == Heap.longitud());
+        assertTrue(Heap.maximo() == 10);
+
+        int i = 1;
+        while (i < Heap.longitud()) {
+            Integer elem = Heap.obtener(i);
+            assertTrue(pertenece(oldHeap, elem));
+            i++;
+        }
+        assertTrue(esColaDePrioridad(Heap));
+    }
+
+    @Test
+    void eliminarVariosElementoArbitrario(){
+        Integer elem1 =Heap.obtener(Heap.longitud()-1);
+        Heap.eliminarPosicion(Heap.longitud()-1);
+        Integer elem2 =Heap.obtener(Heap.longitud()/2);
+        Heap.eliminarPosicion(Heap.longitud()/2);
+
+        assertFalse(pertenece(Heap, elem1));
+        assertFalse(pertenece(Heap, elem2));
+        assertTrue(esColaDePrioridad(Heap));
+    }
+    Integer NCLAVES = 1000;
+    private Integer clave(Integer i) {
+        return NCLAVES * ((i * i - 100 * i) % NCLAVES) + i;
+    }
+
+    @Test
+    void stress() {
+
+        Heap<Integer> conjunto = new Heap<Integer>(new ArrayList<Integer>(),Integer::compare);
+
+        // Insertar
+        for (Integer i = 0; i < NCLAVES; i++) {
+            Integer k = clave(i);
+            assertEquals(false, pertenece(conjunto,k));
+            conjunto.agregar(k);
+            assertEquals(true, pertenece(conjunto,k));
+        }
+        // Insertar de nuevo
+        for (Integer i = 0; i < NCLAVES; i++) {
+            Integer k = clave(i);
+            assertTrue(pertenece(conjunto,k));
+
+            conjunto.agregar(k);
+
+            assertTrue(pertenece(conjunto,k));
+        }
+
+        // Eliminar los valores para i par
+        for (Integer i = 0; i < NCLAVES; i++) {
+            Integer k = clave(i);
+            assertTrue(pertenece(conjunto,k));
+            if (i % 2 == 0) {
+                conjunto.eliminarPosicion(i);
+
+                assertFalse(pertenece(conjunto,k));
+            }
+        }
+        // Eliminar los valores para i impar
+        for (Integer i = 0; i < NCLAVES; i++) {
+            Integer k = clave(i);
+            if (i % 2 == 0) {
+                assertFalse(pertenece(conjunto,k));
+            } else {
+                assertTrue(pertenece(conjunto,k));
+                conjunto.eliminarPosicion(i);
+
+                assertFalse(pertenece(conjunto,k));
+            }
+        }
+        // Verificar que no haya valores
+        for (Integer i = 0; i < NCLAVES; i++) {
+            Integer k = clave(i);
+            assertFalse(pertenece(conjunto,k));
+        }
+    }
+}
