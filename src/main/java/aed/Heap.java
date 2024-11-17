68a63906167a10a9757d2f6fef0cb6c1561de6e9
ucastello <ucastello@dc.uba.ar>
2024-11-17 19:34:26 -0300

    comentarios en el heap

--- src/main/java/aed/Heap.java
+++ src/main/java/aed/Heap.java
@@ -8,14 +8,15 @@ public class Heap<T> {
     private int ultimo;
     private Comparator<T> comparador;
 
-    public Heap (ArrayList<T> info, Comparator<T> com){
+    public Heap (ArrayList<T> info, Comparator<T> com){                         // O(n) 
         comparador = com;
         data = info;
         ultimo = data.size()-1;
-        this.Heapify();
+        this.Heapify();                                                             
     }
 
-    public Heap (Heap<T> h, Comparator<T> comp){                                //Constructor por copia
+    //Constructor por copia
+    public Heap (Heap<T> h, Comparator<T> comp){                                
         comparador = comp;
         ultimo = h.longitud()-1;
         data = new ArrayList<T>();
@@ -26,11 +27,11 @@ public class Heap<T> {
         }
     }
 
-    public T maximo (){
+    public T maximo (){                                                             // O(1)
         return data.get(0);
     }
 
-    public T eliminarPosicion (int i){
+    public T eliminarPosicion (int i){                                              // O(log(n))
         T res = data.get(i);
         if (i == ultimo){
             data.remove(i);
@@ -49,7 +50,7 @@ public class Heap<T> {
             if (!esHoja(i) && hijosConMasPrioridad(i)){
                 bajar(i);
             }
-            else if (comparador.compare(data.get(i), data.get((i-1)/2)) > 0){              //si quiero eliminar la raiz a este else no entra nunca, creo que no habria problema igual
+            else if (comparador.compare(data.get(i), data.get((i-1)/2)) > 0){              
                 subir(i);
             }
         }
@@ -57,26 +58,22 @@ public class Heap<T> {
         return res;
     }
 
-    public int longitud(){
-        return data.size();
+    public int longitud(){                                                // O(1)
+        return data.size();                                                     
     }
 
-    public T obtener(int indice){
+    public T obtener(int indice){                                         // O(1)
         return data.get(indice);
     }
 
-    public void agregar (T elem){
-        if (data.size()== 0){
-            data.add(0,elem);
-            ultimo += 1;
-        } else {
-            data.add(ultimo+1,elem);
-            ultimo +=1;
+    public void agregar (T elem){                                         // O(log(n))
+        data.add(elem);
+        ultimo += 1;
+        if (ultimo != 0){
             subir(ultimo);
         }
-        
     }
-    public void modificarElem (Integer indice, T prioridadAnterior,T prioridadActual){                         //modifica un elemento en O(1) si no se cambia la prioridad 
+    public void modificarElem (Integer indice, T prioridadAnterior,T prioridadActual){                         //modifica un elemento en O(1) si no se cambia la prioridad, si la cambia la complejidad seria de O(log(n))
         data.set(indice, prioridadActual);    
         if (comparador.compare(prioridadAnterior, prioridadActual) > 0){
             bajar(indice);
@@ -86,19 +83,19 @@ public class Heap<T> {
         } 
     }
     
-    public ArrayList<T> heapALista(){                                          //devuelve una lista con elementos con aliasing [en O(n)]             
+    public ArrayList<T> heapALista(){                                          //Devuelve una lista con elementos con aliasing [en O(n)]             
         ArrayList <T> res = new ArrayList<>();
         int i = 0;
-        while ( i < this.data.size()) {
-            res.add(this.data.get(i));
+        while ( i < this.data.size()) {                                        // cantidad de elementos-iteraciones
+            res.add(this.data.get(i));                                         // O(1)
             i++;
         }
         return res;
     }
         
-    private void Heapify(){
+    private void Heapify(){                                                    // O(n)
         int i = data.size()/2-1;
-        while (i >= 0){
+        while (i >= 0){                                                        
             bajar(i);
             i--;
         }
@@ -108,31 +105,26 @@ public class Heap<T> {
         return (2*indice+1 > (data.size()-1));
     }
 
-    private void intercambiar (int Indice1,int Indice2){
+    private void intercambiar (int Indice1,int Indice2){                                // O(1)
         T num1 = data.get(Indice1);
         T num2 = data.get(Indice2);
+        // Si es traslado hay que modificar los punteros, al hacerlo no se modifica la prioridad de ninguno de los elementos asi que la complejidad sigue siendo O(1)
         if (esTraslado(num1)){
             TuplaDeInfo numAInfo1 = (TuplaDeInfo) num1; 
             TuplaDeInfo numAInfo2 = (TuplaDeInfo) num2;
             if (ordenadoPorAntiguedad()){
-                numAInfo1.modificarTupla(numAInfo1.infotras, Indice2,numAInfo1.redit);
+                numAInfo1.modificarTupla(numAInfo1.infotras, Indice2,numAInfo1.redit);                          
                 numAInfo2.modificarTupla(numAInfo2.infotras,Indice1, numAInfo2.redit);
-                T numAct1 = (T) numAInfo1;
-                T numAct2 = (T) numAInfo2;
-                data.set(Indice1, numAct2);
-                data.set(Indice2, numAct1);
-            } else{
+                } 
+            else {
                 numAInfo1.modificarTupla(numAInfo1.infotras, numAInfo1.antig,Indice2);
                 numAInfo2.modificarTupla(numAInfo2.infotras,numAInfo2.antig, Indice1);
-                T numAct1 = (T) numAInfo1;
-                T numAct2 = (T) numAInfo2;
-                data.set(Indice1, numAct2);
-                data.set(Indice2, numAct1);
             }
-        } else{
-            data.set(Indice1, num2);
-            data.set(Indice2, num1);
+            num1 = (T) numAInfo1;
+            num2 = (T) numAInfo2;
         }
+        data.set(Indice1, num2);
+        data.set(Indice2, num1);
     }
 
     private Boolean hijosConMasPrioridad (int indice){                                          // devuelve True si tiene algun hijo con mas prioridad
@@ -142,7 +134,7 @@ public class Heap<T> {
         return ((comparador.compare(data.get(indice), data.get(2*indice+1)) < 0) || (comparador.compare(data.get(indice), data.get(2*indice + 2)) < 0));
     }
 
-    private int hijoMayorPrioridad (int indice){
+    private int hijoMayorPrioridad (int indice){                                    // O(1)                                       
         int res = 2*indice+1;
         if ((2*indice+2 >= data.size())){
             return res;
@@ -153,25 +145,22 @@ public class Heap<T> {
         return res;
     }
 
-    private void bajar(int i){
-        while (!esHoja(i) && hijosConMasPrioridad(i)) {
+    private void bajar(int i){                                                              // O(log(n))
+        while (!esHoja(i) && hijosConMasPrioridad(i)) {                                     // Como maximo hace log(n) iteraciones
             int hijoMayorPrioridad = hijoMayorPrioridad(i);
             intercambiar(i, hijoMayorPrioridad);
             i = hijoMayorPrioridad;
         }
     }
-    
-    private void subir (int i){
-        while (i >1 && (comparador.compare(data.get(i), data.get((i-1)/2))>0)){             
+
+    private void subir (int i){                                                             // O(log(n))
+        while (i > 0 && (comparador.compare(data.get(i), data.get((i-1)/2))>0)){            // Como maximo hace log(n) iteraciones        
             intercambiar(i, (i-1)/2);
             i = (i-1)/2;
         }
-        if (comparador.compare(data.get(1), data.get(0))>0){                
-            intercambiar(1, 0);
-        }
     }
 
-    private boolean esTraslado (T elem){
+    private boolean esTraslado (T elem){                                                      
         TuplaDeInfo prueba = new TuplaDeInfo(null, null, null);
         return (elem.getClass() == prueba.getClass());
     }
